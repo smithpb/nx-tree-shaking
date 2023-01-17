82f2fac383374633cb47f4f9d27a36bcb8f6fd01
smithpb <phillsm2@cisco.com>
2023-01-17 11:37:14 -0600

    Initial commit

---
+++ apps/angular-15-migration-e2e/src/e2e/app.cy.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('angular-15-migration', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome angular-15-migration');
+  });
+});
